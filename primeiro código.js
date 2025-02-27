f4b234d6ffcdf2da1b745c60f30a616ffe94052c
Jp <j0taptdds@outlook.com>
2025-02-27 17:06:17 -0300

    Add files via upload

---
+++ primeiro código.js
@@ -0,0 +1,18 @@
+console.log("Hello Fellas(o)s!!");
+console.log("Aguias são ótimos animais!!");
+
+var nome = "Jotap Lindin";
+console.log(nome)
+console.log(nome + " gosta de viver");
+
+let idade = 26;
+let idadeJoao = 26+1; 
+let somaIdades = idade + idadeJoao;
+
+console.log(somaIdades);
+
+function multiplicarIdades (idade1, idade2) {
+    var multiplicarIdades = idade1 * idade2;
+    return multiplicarIdades
+}
+ console.log(multiplicarIdades (idade, idadeJoao));
